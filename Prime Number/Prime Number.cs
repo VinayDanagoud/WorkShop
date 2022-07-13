6ef92ab9f3fb10cc8246e6b4e94aaa46bfda722a
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-07-13 11:13:02 +0530

    Added Prime Number Program

---
+++ Prime Number/Prime Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    internal class Prime_Number
+    {
+        static void Main(string[] args)
+        {
+            bool isprime = true;
+            Console.WriteLine("Prime Numbers");
+            for (int i = 1; i <= 1000; i++)
+            {
+                for (int j = 1; j <= 1000; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isprime = false;
+                        break;
+                    }
+                }
+                if (isprime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isprime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Prime Number/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("welcome to prime Number ");
+{
+    int n, i, m = 0, sum = 0;
+    Console.WriteLine("Enter the Number to check prime");
+    n = Convert.ToInt32(Console.ReadLine());
+    m = n / 2;
+    for (i = 2; i <= m; i++)
+    {
+        if (n % i == 0)
+        {
+            Console.WriteLine("Number is not prime");
+            sum = 1;
+            break;
+        }
+    }
+    if (sum == 0)
+        Console.WriteLine("Number is prime");
+}
